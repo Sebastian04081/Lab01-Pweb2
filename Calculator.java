301152e4c3d82b9b26ae8dd7364423252298ae08
Jhastyn Jefferson Payehuanca Riquelme <jpayehuancar@unsa.edu.pe>
2023-05-08 22:19:27 -0500

    Aqui estamos multiplicando

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,9 @@
 public class Calculator {
     public static void main(String args[]) {
-        
+     	   
+    }
+    public static int mult(int a, int b){
+	    int mult = a * b;
+	    return mult;
     }
 }
