c24dc44fc95a435173b3d5f9056c3165b92d6e9f
KristenFer <kfernandezca@unsa.edu.pe>
2023-05-08 22:31:10 -0500

    Aqui lo dividimos

--- Calculator.java
+++ Calculator.java
@@ -3,10 +3,10 @@ public class Calculator {
     {
         
     }
-    public static int add(int a ,int b ){
-	    int suma ;
-	    suma = a+b;
-	    return suma;
+    public static int div(int a ,int b ){
+	    int div ;
+	    div = a/b;
+	    return div;
     }
 
 
